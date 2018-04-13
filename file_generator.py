6adb68724c2dafcf45962e00522895e086baf314
R2D1 <dexter.rwl@yandex.ru>
2018-04-14 00:16:21 +0300

    Update file_generator.py

--- file_generator.py
+++ file_generator.py
@@ -6,27 +6,28 @@ from string import *
 from random import *
 #from os import *
 
-# функция, генерирующая текстовые файлы размера 10^n байт
+# функция, генерирующая текстовые файлы заданного размера
 def generate():
-	# генерируем по 50 файлов размера от 10 байт до 10^7 байт
+	# генерируем по 10 файлов размера от 10 байт до 10^7 байт
+	# не претендуем на статистическую полноту
+	# поэтому в демонстрационных целях можно ограничиться десятью файлами каждого размера
 	for i in range(1, 8):
-		for j in range(1, 51):
+		for j in range(1, 11):
 			res = ''
-			file_name = 'file_{}_{}.txt'.format(i, j)
-			with open(file_name, "w") as f:
-			
+			with open('file_{}_{}.txt'.format(i, j), "w") as f:
 				res = ''.join(choice(string.printable) for n in range(10**i))
 				f.write(res)
-				print("File_{}_{}.txt created".format(i, j))
+				print("file_{}_{}.txt created".format(i, j))
 
 
 if __name__ == "__main__":
 
 	old_dir = os.getcwd()
 	working_dir = old_dir + "/files"
-
+	
 	if not os.path.exists(working_dir):
 		res = os.makedirs(working_dir)
-	os.chdir(working_dir)
-	generate()
-	os.chdir(old_dir)
+	os.chdir(working_dir) # переходим в папку files в текущей рабочей директории
+	
+	generate() # ссуществляем генерацию тестовых файлов для проверки архивации
+	os.chdir(old_dir) # возвращаемся в исходную директорию
